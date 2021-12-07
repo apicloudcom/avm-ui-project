a9e6d5ffa00e217970457c0e268f3b30faede0de
yangshjing <yangshjing@yonyou.com>
2021-12-07 16:34:44 +0800

    feat: Input组件开发

---
+++ packages/avm-ui/src/components/input/demos/index.tsx
@@ -0,0 +1,86 @@
+import { Input } from '../input'
+import {List} from '../../list/list'
+import {ListItem} from '../../list/list-item'
+import { DemoBlock } from '../../../demos'
+
+import '../input.less'
+import '../../list/list.less'
+
+class Demo extends Component {
+  install = () => {
+    console.log('Input-Demo');
+  }
+
+  css() {
+    return `
+      .placeholder_class {
+        color: #f00;
+      }
+    `
+  }
+
+  data = {
+    value1: ''
+  }
+
+  render = () => {
+    return (
+      <>
+        <DemoBlock title='基本的输入框组件'>
+          <Input
+            placeholder='请输入内容'
+            value={this.data.value1}
+            onChange={val => {
+              console.log(val)
+            }}
+          />
+        </DemoBlock>
+        <DemoBlock title='带清除按钮'>
+          <Input placeholder='请输入内容' clearable placeholderClass="placeholder_class" onEnterPress={val => {console.log(val)}}  onKeyDown={val => {console.log(val)}}/>
+        </DemoBlock>
+        <DemoBlock title='禁用状态'>
+          <Input placeholder='请输入内容' value='被禁用的输入框' disabled />
+        </DemoBlock>
+        <DemoBlock title='只读状态'>
+          <Input placeholder='请输入内容' value='只读模式的输入框' readOnly />
+        </DemoBlock>
+        <DemoBlock title='上下布局' padding='0' border='none'>
+          <List prefixWidth="100px">
+            <ListItem title='用户名'>
+              <Input placeholder='请输入用户名' clearable />
+            </ListItem>
+            <ListItem title='密码'>
+              <Input placeholder='请输入密码' clearable type='password' id="password1"/>
+            </ListItem>
+          </List>
+        </DemoBlock>
+        <DemoBlock title='左右布局' padding='0' border='none'>
+          <List prefixWidth="100px">
+            <ListItem prefix='用户名'>
+              <Input placeholder='请输入用户名' clearable />
+            </ListItem>
+            <ListItem prefix='密码'>
+              <Input placeholder='请输入密码' clearable type='password' id="password2"/>
+            </ListItem>
+          </List>
+        </DemoBlock>
+        <DemoBlock title='额外的操作按钮' padding='0' border='none'>
+          <List prefixWidth="100px">
+            <ListItem prefix='短信验证码' extra={<text>发送验证码</text>}>
+              <Input placeholder='请输入验证码' clearable />
+            </ListItem>
+          </List>
+        </DemoBlock>
+        <DemoBlock title='右侧对齐'>
+          <Input
+            placeholder='请输入验证码'
+            textAlign="right"
+            clearable
+          />
+        </DemoBlock>
+      </>
+    )
+  }
+}
+
+avm.render(<Demo/>, 'body')
---
+++ packages/avm-ui/src/components/input/input.tsx
@@ -0,0 +1,130 @@
+import classNames from 'classnames'
+import { mergeProps } from '../../utils/with-default-props'
+
+import closeIcon from './img/close.png'
+
+const classPrefix = `adm-input`
+
+
+const defaultProps = {
+  defaultValue: '',
+  maxLength: 500,
+  minLength: 0,
+  fontSize: '17px',
+  color: '#333',
+  placeholderColor: '#ccc',
+  disabledColor: '#999',
+  textAlign: 'left'
+}
+
+export class Input extends Component {
+  install = () => {
+    console.log('Input!')
+  }
+
+  data = {
+    value: '',
+    hasFocus: false,
+    isInit: true
+  }
+
+  setValue = (val, props) => {
+    this.data.value = val;
+    props.onChange?.(val)
+  }
+
+  setHasFocus = state => {
+    this.data.hasFocus = state;
+  }
+
+  render = props => {
+    props = mergeProps(defaultProps, props)
+
+    if(this.data.isInit && props.defaultValue) {
+      this.data.value = props.defaultValue
+    }
+    if(this.data.isInit && props.value) {
+      this.data.value = props.value
+    }
+
+    const handleKeydown = (e: any) => {
+      if (props.onEnterPress && (e.code === 'Enter' || e.keyCode === 13)) {
+        props.onEnterPress(e)
+      }
+      props.onKeyDown?.(e)
+    }
+
+    const inputStyles = {};
+    if (props.fontSize) {
+      inputStyles['fontSize'] = props.fontSize;
+    }
+    if (props.color) {
+      inputStyles['color'] = props.color;
+    }
+    if (props.disabledColor && props.disabled) {
+      inputStyles['color'] = props.disabledColor
+    }
+    if (props.textAlign) {
+      inputStyles['textAlign'] = props.textAlign;
+    }
+
+    return (
+      <div className={`${classPrefix}-wrapper`}>
+        <input
+          id={props.id}
+          className={classNames(classPrefix, {
+            [`${classPrefix}-disabled`]: props.disabled,
+            [`${classPrefix}-readOnly`]: props.readOnly,
+          })}
+          placeholder-class={props.placeholderClass}
+          style={inputStyles}
+          value={this.data.value}
+          onInput={e => {
+            this.setValue(e.target.value, props)
+            props.onInput?.(e)
+          }}
+          onChange={e => {
+            this.setValue(e.target.value, props)
+          }}
+          onFocus={e => {
+            this.setHasFocus(true)
+            props.onFocus?.(e)
+          }}
+          onBlur={e => {
+            this.setHasFocus(false)
+            props.onBlur?.(e)
+          }}
+          placeholder={props.placeholder}
+          disabled={props.disabled}
+          readOnly={props.readOnly}
+          maxLength={props.maxLength}
+          minLength={props.minLength}
+          max={props.max}
+          min={props.min}
+          autoComplete={props.autoComplete}
+          enterKeyHint={props.enterKeyHint}
+          pattern={props.pattern}
+          type={props.type}
+          autoCapitalize={props.autoCapitalize}
+          autoCorrect={props.autoCorrect}
+          onKeyDown={handleKeydown}
+          onKeyUp={props.onKeyUp}
+        />
+        {props.clearable && !!this.data.value && this.data.hasFocus && (
+          <div
+            className={`${classPrefix}-clear`}
+            onMouseDown={e => {
+              e.preventDefault()
+            }}
+            onClick={() => {
+              this.setValue('', props)
+              props.onClear?.()
+            }}>
+            <img src={closeIcon} alt="close" className={`${classPrefix}-clear-icon`}/>
+          </div>
+        )}
+      </div>
+    )
+  }
+  
+}
