c78f6afe49bfcd9aac38c2823f6386e4b52ed375
lixuanyu <lixuany@yonyou.com>
2021-12-20 09:47:41 +0800

    feat: image-viewer组件开发
    loading,mask组件优化

---
+++ packages/avm-ui/src/components/image-viewer/index.ts
@@ -0,0 +1,13 @@
+import './image-viewer.less'
+// import { attachPropertiesToComponent } from '../../utils/attach-properties-to-component'
+import {
+  ImageViewer,
+} from './image-viewer'
+
+export type { ImageViewerProps } from './image-viewer'
+
+// const Multi = attachPropertiesToComponent(MultiImageViewer, {
+//   show: showMultiImageViewer,
+// })
+
+export default ImageViewer
