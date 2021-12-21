26540713d672fff51e5134721b376aa41fc334e1
yangshjing <yangshjing@yonyou.com>
2021-12-21 11:48:28 +0800

    feat: toast组件-待完善

---
+++ packages/avm-ui/src/components/toast/methods.tsx
@@ -0,0 +1,66 @@
+import { resolveContainer } from '../../utils/get-container'
+import { InternalToast, ToastProps } from './toast'
+import { mergeProps } from '../../utils/with-default-props'
+
+const containers = [] as HTMLDivElement[]
+
+// 卸载移除元素
+function unmount(container: HTMLDivElement) {
+  console.log('unmount container=', container)
+  // const unmountResult = ReactDOM.unmountComponentAtNode(container)
+  // if (unmountResult && container.parentNode) {
+  //   container.parentNode.removeChild(container)
+  // }
+}
+
+export type ToastShowProps = Omit<ToastProps, 'visible'>
+
+const defaultProps = {
+  duration: 2000,
+}
+
+export function show(p: any) {
+  const props = mergeProps(defaultProps, typeof p === 'string' ? { content: p } : p)
+  let timer = 0
+  let visible = true
+
+  const setVisible = v => {
+    visible = v
+  }
+
+  const {getContainer = () => document.body} = props
+  const container = document.createElement('div')
+  const bodyContainer = resolveContainer(getContainer)
+  bodyContainer.appendChild(container)
+  clear()
+  containers.push(container)
+
+  const TempToast = () => {
+    if (props.duration === 0) {
+      window.clearTimeout(timer)
+    }
+    timer = window.setTimeout(() => {
+      setVisible(false)
+      props.afterClose?.()
+    }, props.duration)
+    return (
+      <InternalToast
+        {...props}
+        getContainer={() => container}
+        visible={visible}
+        afterClose={() => {
+          unmount(container)
+        }}
+      />
+    )
+  }
+  avm.render(<TempToast />, 'div')
+}
+
+export function clear() {
+  while (true) {
+    const container = containers.pop()
+    if (!container) break
+    unmount(container)
+  }
+}
---
+++ packages/avm-ui/src/components/toast/toast.tsx
@@ -0,0 +1,76 @@
+import classNames from 'classnames'
+import { CheckOutline, CloseOutline } from '../icon/icon'
+import Loading from '../loading'
+import Mask from '../mask'
+import { mergeProps } from '../../utils/with-default-props'
+import { GetContainer } from '../../utils/render-to-container'
+import { PropagationEvent } from '../../utils/with-stop-propagation'
+
+const classPrefix = `adm-toast`
+
+export interface ToastProps {
+  afterClose?: () => void
+  maskStyle?: HTMLElement
+  maskClassName?: string
+  maskClickable?: boolean
+  content?: HTMLElement
+  icon?: 'success' | 'fail' | 'loading' | HTMLElement
+  duration?: number
+  position?: 'top' | 'bottom' | 'center'
+  visible?: boolean
+  getContainer?: GetContainer
+  stopPropagation?: PropagationEvent[]
+}
+
+const iconsArr = {
+  success: <CheckOutline/>, 
+  fail: <CloseOutline/>, 
+  loading: <Loading color='white' className={`${classPrefix}-loading`} />, 
+}
+
+const top =  {
+  top: '20%',
+  bottom: '80%'
+}
+
+const defaultProps = {
+  maskClickable: true,
+  stopPropagation: ['click'],
+}
+
+export class InternalToast extends Component {
+  render = props => {
+    props = mergeProps(defaultProps, props)
+
+    const { maskClickable, content, icon, position='50%' } = this.props
+
+    console.log(position)
+    return (
+      <Mask
+        visible={props.visible}
+        destroyOnClose
+        opacity={0}
+        disableBodyScroll={!maskClickable}
+        getContainer={props.getContainer}
+        afterClose={props.afterClose}
+        style={{
+          pointerEvents: maskClickable ? 'none' : 'auto',
+          ...props.maskStyle,
+        }}
+        className={classNames(`${classPrefix}-mask`, props.maskClassName)}
+        stopPropagation={props.stopPropagation}
+      >
+        <div
+          style={{top: ['top', 'bottom'].includes(position) ? top[position] : position}}
+          className={classNames(
+            `${classPrefix}-wrap`,
+            icon ? `${classPrefix}-wrap-icon` : `${classPrefix}-wrap-text`
+          )}
+        >
+          <div className={`${classPrefix}-icon`}>{iconsArr[icon] || icon}</div>
+          <text>{content}</text>
+        </div>
+      </Mask>
+    )
+  }
+}
