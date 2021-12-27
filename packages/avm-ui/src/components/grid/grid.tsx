98258f01267d80c94cddc5eaac98d61cde5f286a
yangshjing <yangshjing@yonyou.com>
2021-12-27 15:47:02 +0800

    feat: grid组件更改

--- packages/avm-ui/src/components/grid/grid.tsx
+++ packages/avm-ui/src/components/grid/grid.tsx
@@ -4,47 +4,61 @@ import { toCSSLength } from '../../utils/to-css-length'
 const classPrefix = `adm-grid`
 
 export class Grid extends Component {
-  install = () => {
-    console.log('Grid!')
-  }
-
   render = props => {
     const {gap=0, columns} = props
-    const styles = {}
-    styles['grid-template-columns'] = `repeat(${columns.toString()}, minmax(0, 1fr))`
-    
-    if (Array.isArray(gap)) {
-      styles['column-gap'] = toCSSLength(gap[0])
-      styles['row-gap'] = toCSSLength(gap[1])
-    } else {
-      styles['column-gap'] = toCSSLength(gap)
-      styles['row-gap'] = toCSSLength(gap)
-    }
-  
+    // const styles = {}
+    // styles['grid-template-columns'] = `repeat(${columns.toString()}, minmax(0, 1fr))`
+      
+    // if (Array.isArray(gap)) {
+    //   styles['column-gap'] = toCSSLength(gap[0])
+    //   styles['row-gap'] = toCSSLength(gap[1])
+    // } else {
+    //   styles['column-gap'] = toCSSLength(gap)
+    //   styles['row-gap'] = toCSSLength(gap)
+    // }
+
+    const baseWidth = 100 / columns
+    const columnGap = Array.isArray(gap) ? toCSSLength(gap[0]) : toCSSLength(gap)
+    const rowGap = Array.isArray(gap) ? toCSSLength(gap[1]) : toCSSLength(gap)
     return (
-      <div className={classPrefix} style={styles}>
-        {props.children}
+      // <div className={classPrefix} style={styles}>
+      //   {props.children}
+      // </div>
+
+      <div className={classPrefix}>
+        {props.children.map((item, index) => {
+          return (
+            <GridItem
+              {...item.attributes}
+              baseWidth={baseWidth}
+              columnGap={columnGap}
+              rowGap={rowGap}
+              className={`${classPrefix}-item`}
+              columns={columns}
+              curIndex={index + 1}>{item}</GridItem>
+          )
+        })}
       </div>
     )
   }
 }
 
 export class GridItem extends Component {
-  install = () => {
-    console.log('GridItem!')
-  }
-
   render = props => {
     props = mergeProps({span: 1}, props)
+    // const itemStyle = {}
+    // itemStyle['grid-column-end'] = `span ${props.span}`
 
-    const itemStyle = {}
-    itemStyle['grid-column-end'] = `span ${props.span}`
+    const itemStyle = {
+      width: `${props.baseWidth * props.span}%`,
+      // marginRight: props.columnGap,
+      // marginBottom: props.rowGap
+    }
     return (
       <div
-        className={`${classPrefix}-item`}
+        // className={`${classPrefix}-item`}
         style={itemStyle}
-        onClick={props.onClick}
-      >
+        onClick={props.onClick}>
         {props.children}
       </div>
     )
