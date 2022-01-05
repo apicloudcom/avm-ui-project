8ab63ba2d9f59708d377e44cac771a9171cb7afe
YangYongAn <yangyongan@yonyou.com>
2022-01-05 11:54:25 +0800

    chore: 调整按需引用构建模式

---
+++ packages/cli/src/commands/widget.js
@@ -0,0 +1,109 @@
+import fse from 'fs-extra';
+import {resolve} from 'path';
+import cp from 'child_process';
+
+
+export async function onWidget() {
+
+  // 1. 重置输出目录
+  fse.ensureDirSync(dist);
+  fse.emptyDirSync(dist);
+  // 2. 创建 config.xml
+  fse.outputFileSync(resolve(dist, 'config.xml'), createConfigXml());
+  // 3. 创建 components 到 widget
+  buildComponentToWidget();
+  // 4. 创建pages
+  await createPagesFromDemos();
+}
+
+import xml2js from 'xml2js';
+
+function createConfigXml() {
+  const builder = new xml2js.Builder();
+
+
+  return builder.buildObject({
+    widget: {
+      $: {id: "A6183702447528", version: "0.0.1"},
+      content: {$: {"src": "pages/main/main.stml"}},
+      name: "AVM-UI",
+      description: "Example For APICloud.",
+      author: {
+        _: "Developer",
+        $: {"email": "developer@apicloud.com", "href": "http://www.apicloud.com"}
+      },
+      access: {$: {"origin": "*"}},
+      preference: [
+        {$: {name: "avm", value: true}},
+        {$: {name: "pageBounce", value: false}},
+        {$: {name: "appBackground", value: "#FFF"}},
+        {$: {name: "windowBackground", value: "rgba(0,0,0,0.0)"}},
+        {$: {name: "frameBackgroundColor", value: "rgba(0,0,0,0.0)"}},
+        {$: {name: "hScrollBarEnabled", value: false}},
+        {$: {name: "vScrollBarEnabled", value: false}},
+        {$: {name: "autoLaunch", value: true}},
+        {$: {name: "fullScreen", value: false}},
+        {$: {name: "autoUpdate", value: true}},
+        {$: {name: "smartUpdate", value: true}},
+        {$: {name: "debug", value: true}},
+        {$: {name: "statusBarAppearance", value: true}},
+        {$: {name: "font", family: "avm-ui-icon", value: "widget/components/avm-ui/avm-ui-icon.ttf"}},
+      ],
+      permission: [
+        {$: {name: "readPhoneState"}},
+        {$: {name: "camera"}},
+        {$: {name: "record"}},
+        {$: {name: "location"}},
+        {$: {name: "fileSystem"}},
+        {$: {name: "internet"}},
+        {$: {name: "bootCompleted"}},
+        {$: {name: "hardware"}}
+      ]
+    }
+  });
+}
+
+function buildComponentToWidget() {
+
+  try {
+
+    const cmd = `cd ${uiDir} && tsc && vite build --mode single`;
+    console.log(`running ${cmd}`);
+    const data = cp.execSync(cmd);
+    console.log(data.toString());
+    const dest = `${dist}/components`;
+    fse.copySync(`${uiDir}/widget/components`, dest, {});
+    console.log(`components copied to ${dest}`);
+  } catch (e) {
+    console.log("buildComponentToWidget error", e);
+  }
+
+}
+
+import glob from 'glob'
+import {dist, uiDir} from "../index.js";
+
+const assetsLoader = {
+  name: 'assets',
+  setup(build) {
+    build.onResolve({filter: /\.png|\.svg$/}, args => {
+      console.log(args)
+      return {
+        path: 'a.png',
+        external: true,
+      }
+    })
+  },
+}
+
+async function createPagesFromDemos() {
+  const demos = glob.sync("src/components/*/demos/*.tsx", {cwd: resolve(uiDir)});
+
+  const files = demos.map(demo => resolve(uiDir, demo));
+
+  console.log(files)
+
+
+}
+
+
