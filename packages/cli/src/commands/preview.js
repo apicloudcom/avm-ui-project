99f2b1c5bd9f7f678c71a8ebfae0b6f7438f14a3
YangYongAn <yangyongan@yonyou.com>
2022-01-11 21:29:21 +0800

    fixed: 修复 preview 路径问题

--- packages/cli/src/commands/preview.js
+++ packages/cli/src/commands/preview.js
@@ -5,11 +5,11 @@ import {dist, uiDir} from "../index.js";
 export function onPreview(cmd) {
   if (process.argv[3]) {
     const name = process.argv[3];
-    const srcPath = resolve(uiDir, `widget/pages/demo-button/demo-${name}.stml`);
+    const srcPath = resolve(uiDir, `widget/pages/demo-${name}/demo-${name}.stml`);
     if (fse.existsSync(srcPath)) {
       fse.copySync(srcPath, resolve(dist, 'pages/main/main.stml'), {})
     } else {
-      throw new Error("文件找不到" + srcPath,)
+      throw new Error(`demo 页面 ${name} 找不到: ${srcPath}`);
     }
   } else {
     throw Error("请传入页面");
