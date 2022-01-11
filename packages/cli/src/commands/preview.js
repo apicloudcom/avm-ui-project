58f990b3ae421fd4a13a1ab53d54b1510728acc0
YangYongAn <yangyongan@yonyou.com>
2022-01-11 21:25:49 +0800

    feat: avm-ui W;avm-ui B;avm-ui P button;

--- packages/cli/src/commands/preview.js
+++ packages/cli/src/commands/preview.js
@@ -1,8 +1,16 @@
-
+import fse from "fs-extra";
+import {resolve} from "path";
+import {dist, uiDir} from "../index.js";
 
 export function onPreview(cmd) {
   if (process.argv[3]) {
-    
+    const name = process.argv[3];
+    const srcPath = resolve(uiDir, `widget/pages/demo-button/demo-${name}.stml`);
+    if (fse.existsSync(srcPath)) {
+      fse.copySync(srcPath, resolve(dist, 'pages/main/main.stml'), {})
+    } else {
+      throw new Error("文件找不到" + srcPath,)
+    }
   } else {
     throw Error("请传入页面");
   }
