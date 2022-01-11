9eeb500957f4fbce68700ccf201018b0f619ffe0
朱天昊 <zhuth12@yonyou.com>
2022-01-11 15:39:45 +0800

    feat:展开和收起按钮变成链接样式，demo和官网一样

--- packages/avm-ui/src/components/ellipsis/ellipsis.tsx
+++ packages/avm-ui/src/components/ellipsis/ellipsis.tsx
@@ -21,22 +21,33 @@ export class Ellipsis extends Component {
   }
   // 初始化显示内容
   installed = () => {
-    const actionText = this.data.expanded ? this.props.collapseText : this.props.expandText
-    const leading = this.props.content.slice(0, 6) + '...';
-    const tailing = '...' + this.props.content.slice(-6);
+    const actionText = this.data.expanded ?
+      <span style={{ "color": "blue" }}>{this.props.collapseText}</span> :
+      <span style={{ "color": "blue" }}>{this.props.expandText}</span>
+    const leading = this.props.content.slice(0, 12) + '...';
+    const tailing = '...' + this.props.content.slice(-12);
 
-    if (this.props.direction === 'start') {
-      this.data.content = actionText + '...' + this.props.content.slice(-12) 
-      return this.data.content
-    } else if (this.props.direction === 'middle'){
-      this.data.content = leading + '...' + actionText + '...' + tailing
-      return this.data.content
-     } else if (this.props.rows ){
-      this.data.content = this.props.content.slice(0,this.props.rows*28) + "..."
-      return this.data.content
+    if (this.props.rows) {
+      this.data.content = this.props.content.slice(0, this.props.rows * 28) + "..."
+    } else if (this.props.direction === 'start') {
+      this.data.content =
+        <span>
+          {actionText}
+          <span>{"..." + this.props.content.slice(-26)}</span>
+        </span>
+    } else if (this.props.direction === 'middle') {
+      this.data.content =
+        <span>
+          <span>{leading}</span>
+          {actionText}
+          <span>{tailing}</span>
+        </span>
     } else {
-      this.data.content = this.props.content.slice(0,12) + "..." + actionText
-      return this.data.content
+      this.data.content =
+        <span>
+          <span>{this.props.content.slice(0, 26) + "..."}</span>
+          {actionText}
+        </span>
     }
   }
 
@@ -52,54 +63,74 @@ export class Ellipsis extends Component {
   render = props => {
     props = mergeProps(defaultProps, props)
     const that = this
-    const actionText = !this.data.expanded ? props.collapseText : props.expandText
+    const actionText = !this.data.expanded ?
+      <span style={{ "color": "blue" }}>{this.props.collapseText}</span> :
+      <span style={{ "color": "blue" }}>{this.props.expandText}</span>
 
     const expandActionElement =
-        <a style= "border: 1px solid white"
-          onClick={() => {
-            this.setExpanded(true)
-            if (props.direction === 'start') {
-              that.data.content =   props.content + actionText
-              return that.data.content
-            } else if (props.direction === 'middle') {
-              that.data.content = props.content + actionText
-              return that.data.content
-            } else {
-              that.data.content = props.content +  actionText
-              return that.data.content
-            }
-          }}
-        >
-          {" "}
-        </a>
+      <a style="border: 1px solid white"
+        onClick={() => {
+          this.setExpanded(true)
+          if (props.direction === 'start') {
+            that.data.content =
+              <span>
+                <span>{props.content}</span>
+                {actionText}
+              </span>
+          } else if (props.direction === 'middle') {
+            that.data.content =
+              <span>
+                <span>{props.content}</span>
+                {actionText}
+              </span>
+          } else {
+            that.data.content =
+              <span>
+                <span>{props.content}</span>
+                {actionText}
+              </span>
+          }
+        }}
+      >
+        {" "}
+      </a>
 
     const collapseActionElement =
-        <a style= "border: 1px solid white"
-          onClick={() => {
-            this.setExpanded(false)
-              const leading = props.content.slice(0, 6) + '...';
-              const tailing = '...' + props.content.slice(-6);
-        
-              if (props.direction === 'start') {
-                that.data.content = actionText + '...' + props.content.slice(-12) + "3333"
-                return that.data.content
-              } else if (props.direction === 'middle') {
-                that.data.content = leading + '...' + actionText + '...' + tailing
-                return that.data.content
-              } else {
-                that.data.content = props.content.slice(0,12) + "..." + actionText
-                return that.data.content
-              }
-          }}
-        >
-          {" "}
-        </a>
-      
+      <a style="border: 1px solid white"
+        onClick={() => {
+          this.setExpanded(false)
+          const leading = props.content.slice(0, 12) + '...';
+          const tailing = '...' + props.content.slice(-12);
+
+          if (this.props.direction === 'start') {
+            this.data.content =
+              <span>
+                {actionText}
+                <span>{"..." + this.props.content.slice(-26)}</span>
+              </span>
+          } else if (this.props.direction === 'middle') {
+            this.data.content =
+              <span>
+                <span>{leading}</span>
+                {actionText}
+                <span>{tailing}</span>
+              </span>
+          } else {
+            this.data.content =
+              <span>
+                <span>{this.props.content.slice(0, 26) + "..."}</span>
+                {actionText}
+              </span>
+          }
+        }}
+      >
+        {" "}
+      </a>
+
     const renderContent = () => {
-      if (props.content.length <= 20) {
+      if (props.content.length <= 28) {
         return props.content
       }
-      // 如果展开
       if (this.data.expanded) {
         return (
           <>
