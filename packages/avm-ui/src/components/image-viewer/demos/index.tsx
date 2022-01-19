e12d26dce1fceb2e6d51d830f19007075f7ca144
lixuanyu <lixuany@yonyou.com>
2022-01-19 11:40:48 +0800

    feat:新增widget下demo-image-viewer文件

--- packages/avm-ui/src/components/image-viewer/demos/index.tsx
+++ packages/avm-ui/src/components/image-viewer/demos/index.tsx
@@ -3,8 +3,8 @@ import ImageViewer from '../index'
 import {DemoBlock} from "../../../demos";
 
 const demoImages = [
-  'https://images.unsplash.com/photo-1601128533718-374ffcca299b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3128&q=80',
-  'https://images.unsplash.com/photo-1567945716310-4745a6b7844b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3113&q=80',
+  'https://img95.699pic.com/photo/50033/7459.jpg_wh860.jpg',
+  'https://img95.699pic.com/photo/50033/7459.jpg_wh860.jpg',
   'https://images.unsplash.com/photo-1624993590528-4ee743c9896e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=200&h=1000&q=80',
 ]
 
