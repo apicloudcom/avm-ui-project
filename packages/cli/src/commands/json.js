9156684db3a890eba427eef8d5d403a7a505a52d
YangYongAn <yangyongan@yonyou.com>
2022-02-24 11:54:22 +0800

    fixed: 检测 json 格式问题

--- packages/cli/src/commands/json.js
+++ packages/cli/src/commands/json.js
@@ -3,27 +3,31 @@ import {resolve} from "path";
 import {uiDir} from "../index.js";
 import fse from "fs-extra";
 
-export function onJSON(){
+export function onJSON() {
   const jsons = glob.sync(`${resolve(uiDir)}/src/components/*/*.json`,);
   const jsonMap = {};
-  
-  jsons.forEach(json=>{
-    const name = json.replace('.json','').split('/').pop();
+
+  jsons.forEach(json => {
+    const name = json.replace('.json', '').split('/').pop();
     try {
-      jsonMap[name] = JSON.parse(fse.readFileSync(json).toString());
-    }catch (e){
-      console.log(`出错了`,name)
+      if (/[A-Z]/.test(name)) {
+        console.log(`${name} 组件名称不符合规范，包含了大写字母`)
+      } else {
+        jsonMap[`avm-${name}`] = JSON.parse(fse.readFileSync(json).toString());
+      }
+    } catch (e) {
+      console.log(`出错了`, name)
     }
   })
 
   // console.log(jsonMap)
   try {
-    const savePath = resolve(uiDir,'../../','avm-ui.json');
+    const savePath = resolve(uiDir, '../../', 'avm-ui.json');
 
-    fse.writeJsonSync(savePath,jsonMap);
-    console.log(`文件输出在${savePath}，文件大小：${fse.statSync(savePath).size/1000} KB`)
-    
-  }catch (e){
+    fse.writeJsonSync(savePath, jsonMap);
+    console.log(`文件输出在${savePath}，文件大小：${fse.statSync(savePath).size / 1000} KB`)
+
+  } catch (e) {
     console.log(e)
   }
 }
