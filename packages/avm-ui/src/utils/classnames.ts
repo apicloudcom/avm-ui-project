652a7b1c7976410304139872c9938adb366edb11
YangYongAn <yangyongan@yonyou.com>
2022-03-09 18:32:55 +0800

    add: 更新简化版 classnames

---
+++ packages/avm-ui/src/utils/classnames.ts
@@ -0,0 +1,19 @@
+/**
+ * 返回 class 样式字符串
+ * @param args
+ */
+export default function classNames(...args): string {
+  const cls: string[] = [];
+  args.forEach(arg => {
+    if (typeof arg === 'string') {
+      cls.push(arg)
+    } else if (Object.prototype.toString.call(arg) === '[object Object]') {
+      for (const argKey in arg) {
+        arg[argKey] && cls.push(argKey);
+      }
+    } else {
+      console.log(`arg ${arg} type not support`);
+    }
+  })
+  return cls.join(' ');
+}
