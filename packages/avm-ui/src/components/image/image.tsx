ee7176fba7d4cb1d9dafeb7e91a200396a67703d
yangshjing <yangshjing@yonyou.com>
2022-03-17 10:11:32 +0800

    feat: image组件调整

--- packages/avm-ui/src/components/image/image.tsx
+++ packages/avm-ui/src/components/image/image.tsx
@@ -7,13 +7,13 @@ const classPrefix = `adm-image`
 const defaultProps = {
   fit: 'fill',
   placeholder: (
-    <div className={`${classPrefix}-tip`}>=
-      <PictureOutline className={`${classPrefix}-tip-icon`}/>
+    <div className={`${classPrefix}-tip`}>
+      <PictureOutline color="#ddd"/>
     </div>
   ),
   fallback: (
     <div className={`${classPrefix}-tip`}>
-      <PictureWrongOutline className={`${classPrefix}-tip-icon`}/>
+      <PictureWrongOutline color="#ddd"/>
     </div>
   )
 }
@@ -43,7 +43,11 @@ export class Image extends Component {
     const style = {};
     style['width'] = toCSSLength(width)
     style['height'] = toCSSLength(height)
-    isRound && (style['borderRadius'] = '50%')
+    if (isRound) {
+      style['borderRadius'] = '50%'
+      style['width'] = '120px'
+      style['height'] = '120px'
+    }
     if (isSquare) {
       style['borderRadius'] = '4px'
       style['width'] = '120px'
@@ -115,15 +119,10 @@ export class Image extends Component {
       .adm-image-tip {
         position: relative;
         background-color: #f3f3f3;
+        width: 100%;
         height: 100%;
-        min-height: 24px;
-        min-width: 24px;
-      }
-      .adm-image-tip-icon {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
+        align-items: center;
+        justify-content: center;
       }
       .adm-image-placeholder-wrapper {
         width: 100%;
