2d564bb079fe1afeebc3b53a1d6a64e408caf8c3
YangYongAn <yangyongan@yonyou.com>
2022-03-17 21:12:38 +0800

    chore: 去掉多余信息

--- packages/avm-ui/src/demos/index.ts
+++ packages/avm-ui/src/demos/index.ts
@@ -1,4 +1,4 @@
-export { lorem } from './utils/lorem'
+// export { lorem } from './utils/lorem'
 export { DemoBlock } from './demo-block'
 export { DemoDescription } from './demo-description'
 export { createPropsTable } from './create-props-table'
