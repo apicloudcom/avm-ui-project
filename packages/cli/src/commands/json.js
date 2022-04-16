f1a3a094d626a268c2e0a1e0461da39bcbd4a2c8
YangYongAn <yangyongan@yonyou.com>
2022-04-16 15:02:09 +0800

    update: 修正部分 json 和 json 生成规则

--- packages/cli/src/commands/json.js
+++ packages/cli/src/commands/json.js
@@ -3,11 +3,12 @@ import {resolve} from "path";
 import {uiDir} from "../index.js";
 import fse from "fs-extra";
 
+const IDEPrefix = 'Ant';
+const ComponentPrefix = 'avm';
+
 export function onJSON() {
   const jsons = glob.sync(`${resolve(uiDir)}/src/components/!(icon)/**.json`);
   const jsonMap = {};
-  const IDEPrefix = 'Ant';
-  const ComponentPrefix = 'avm';
 
   jsons.forEach(json => {
     const name = json.replace('.json', '').split('/').pop();
@@ -17,9 +18,7 @@ export function onJSON() {
       } else {
         const jsonContent = JSON.parse(fse.readFileSync(json).toString());
         jsonContent.component = IDEPrefix + jsonContent.component;
-        if (jsonContent.parent) {
-          jsonContent.parent = ComponentPrefix + '-' + jsonContent.parent;
-        }
+        processContent(jsonContent);
         jsonMap[`${ComponentPrefix}-${name}`] = jsonContent;
       }
     } catch (e) {
@@ -37,4 +36,42 @@ export function onJSON() {
   } catch (e) {
     console.log(e)
   }
+}
+
+
+function processContent(jsonContent) {
+  if (jsonContent.parent) {
+    jsonContent.parent = ComponentPrefix + '-' + jsonContent.parent;
+  }
+
+  jsonContent.props = jsonContent.props.map(prop => {
+    const desc = `[${jsonContent.component}] 的 [${prop.name}]`;
+    if (prop.type) {
+      if (~prop.type.indexOf('[]')) {
+        console.warn(`${desc} 的 type 不规范的数组，缺省为 array 。`);
+        prop.type = 'array';// 修改为 string
+      } else if (prop.type === 'CSSProperties') {
+        console.warn(`${desc} 的 type 是 CSSProperties，缺省为 string 。`);
+        prop.type = 'string';// 修改为 string
+      } else if (prop.type === 'Node') {
+        console.warn(`${desc} 的 type 是 Node，缺省为 string 。`);
+        prop.type = 'string';// 修改为 string
+      }
+    } else {
+      console.warn(`${desc} 的 type 未声明，缺省为 string 。`);
+      prop.type = 'string';// 修改为 string
+    }
+
+    if (jsonContent.event) {
+      jsonContent.event = jsonContent.event.map(item => {
+        if (item.startsWith('on')) {
+          console.warn(`${desc} 的 event ${item} 是以 on 开头的，应该去掉 on 。`);
+          item = item.slice(2);
+        }
+        return item;
+      })
+    }
+
+    return prop;
+  })
 }
