fe22e183264486784dcdd551be2aa4962cd48a4d
YangYongAn <yangyongan@yonyou.com>
2022-05-09 07:18:44 +0800

    update: 注册 tabbar 标签

--- packages/avm-ui/src/components/tab-bar/index.ts
+++ packages/avm-ui/src/components/tab-bar/index.ts
@@ -1,10 +1,13 @@
-import { TabBar, TabBarItem } from './tab-bar'
-import { attachPropertiesToComponent } from '../../utils/attach-properties-to-component'
-export type { TabBarProps } from './tab-bar'
+import {TabBar, TabBarItem} from './tab-bar'
+import {attachPropertiesToComponent} from '../../utils/attach-properties-to-component'
+
+export type {TabBarProps} from './tab-bar'
 
 export default attachPropertiesToComponent(TabBar, {
-    Item: TabBarItem,
-  })
+  Item: TabBarItem,
+})
 
-  avm.define('avm-tabbar', TabBar)
-  avm.define('avm-tabbar-item', TabBarItem)
+avm.define('avm-tabbar', TabBar)
+avm.define('avm-tabbar-item', TabBarItem)
+avm.define('avm-tab-bar', TabBar)
+avm.define('avm-tab-bar-item', TabBarItem)
