59d27d34df917b1b2eaa16e10fb754fa56ad658e
YangYongAn <yangyongan@yonyou.com>
2022-05-25 21:30:24 +0800

    chore: 重构 badge 样式和属性继承

--- packages/avm-ui/src/components/badge/badge.tsx
+++ packages/avm-ui/src/components/badge/badge.tsx
@@ -1,5 +1,6 @@
 import {formatLabel} from '../../utils/format-label'
 import {superProps} from "../../utils/superProps";
+import css from './badge.less';
 
 const classPrefix = `adm-badge`
 
@@ -44,39 +45,5 @@ export class Badge extends Component {
     )
   }
 
-  css = () => {
-    return `
-    
-    .adm-badge-row{flex-flow:row;}
-    
-      .adm-badge-wrap {
-        position: relative;
-        margin:10px;
-      }
-      .adm-badge {
-        color: #fff;
-        font-weight: normal;
-        text-align: center;
-        vertical-align: middle;
-        box-sizing: border-box;
-        min-width: 16px;
-        border-radius: 100px;
-        padding: 0 4px;
-        font-size: 10px;
-    line-height: 16px;
-  height: 16px;
-        white-space: nowrap;
-      }
-      .adm-badge--fixed {
-        position: absolute;
-        transform: translate(50%, -50%);
-      }
-      .adm-badge--dot {
-        min-width: 8px;
-        width: 8px;
-        height: 8px;
-        border-radius: 5px;
-      }
-    `
-  }
+  css = () => css
 }
